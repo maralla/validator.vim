1abf25e5042b6546399bfcf38ecaca7cff26cb52
maralla <maralla.ai@gmail.com>
2015-03-10 17:01:05 +0800

    set current working directory

--- fixup/checkers/__init__.py
+++ fixup/checkers/__init__.py
@@ -3,7 +3,10 @@
 from __future__ import absolute_import
 
 import collections
+import contextlib
+import shlex
 import subprocess
+import os
 import os.path
 
 
@@ -12,6 +15,15 @@ def safe_str(lst):
     for e in lst:
         r.append('"{}"'.format(e.replace('"', '\\"')))
     return '[{}]'.format(','.join(r))
+
+
+@contextlib.contextmanager
+def cwd(chd):
+    old_cwd = os.getcwd()
+    try:
+        yield os.chdir(chd)
+    finally:
+        os.chdir(old_cwd)
 
 
 class Meta(type):
@@ -46,30 +58,35 @@ class SyntaxChecker(Base):
             raise Exception("No checker")
 
         fpath = vim.eval("expand('%:p')")
-        fdir = os.path.dirname(fpath)
 
         r = cls.filter_file(fpath)
         if not r:
             return []
 
-        res = subprocess.Popen(cls.cmd(fpath), shell=True, cwd=fdir,
-                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out = res.communicate()
-        out = '\n'.join(out)
-        err_lines = out.strip().replace('\r', '').split('\n')
+        cmd_args = shlex.split(cls.cmd(os.path.basename(fpath)))
 
-        if cls.errorformat:
-            vim.command("let old_fmt = &errorformat")
-            vim.command("let &errorformat = '{}'".format(cls.errorformat))
+        with cwd(os.path.dirname(fpath)):
+            res = subprocess.Popen(cmd_args, stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+            out = res.communicate()
 
-        vim.command("let expr_str = {}".format(safe_str(err_lines)))
-        vim.command("lgetexpr expr_str")
-        vim.command("unlet expr_str")
-        errors = vim.eval("getloclist(0)")
+            err_lines = '\n'.join(out).\
+                strip().\
+                replace('\r', '').\
+                split('\n')
 
-        if cls.errorformat:
-            vim.command("let &errorformat = old_fmt")
-            vim.command("unlet old_fmt")
+            if cls.errorformat:
+                vim.command("let old_fmt = &errorformat")
+                vim.command("let &errorformat = '{}'".format(cls.errorformat))
+
+            vim.command("let expr_str = {}".format(safe_str(err_lines)))
+            vim.command("lgetexpr expr_str")
+            vim.command("unlet expr_str")
+            errors = vim.eval("getloclist(0)")
+
+            if cls.errorformat:
+                vim.command("let &errorformat = old_fmt")
+                vim.command("unlet old_fmt")
 
         loclist = []
         for i, e in enumerate(errors):
