703373743abf57df68ad747b32e94f9448746afb
Manpreet Singh <junkblocker@yahoo.com>
2016-10-06 10:09:27 -0700

    Add zsh -n lint

---
+++ tests/test_zsh.py
@@ -0,0 +1,17 @@
+import json
+from lints.zsh import ZshLint
+
+
+def test_zsh():
+    msg = [
+        '.zshrc:598: no such file or directory: /etc/ssh/ssh_known_hosts2',
+    ]
+
+    res = ZshLint.parse_loclist(msg, 1)
+    assert json.loads(res[0]) == {
+        "lnum": "598",
+        "bufnr": 1,
+        "enum": 1,
+        "text": '[zsh]no such file or directory: /etc/ssh/ssh_known_hosts2',
+        "type": "E",
+    }
