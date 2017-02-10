071bfcf0264de804afbb71b61839b06b20bbbda3
Ethan Chan <metakirby5@gmail.com>
2017-02-10 15:42:46 -0800

    add coffeelint

---
+++ pythonx/lints/coffee/coffeelint.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from validator import Validator
+
+
+class CoffeeLint(Validator):
+    __filetype__ = "coffee"
+
+    stdin = True
+    checker = "coffeelint"
+    args = " --stdin --reporter csv"
+    regex = r"""
+            .+?,
+            (?P<lnum>\d+),
+            (?P<col>\d*),
+            (
+                (?P<error>error)
+                |
+                (?P<warning>warn)
+            ),
+            (?P<text>.*)"""
